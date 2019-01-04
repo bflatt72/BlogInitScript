125895aaef8c00c690b1aac745e6c9d3d3478cd8
Brian Flatt <FlattDev@Brians-MacBook.local>
2019-01-03 21:16:54 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a Python script that I wrote that automates the creation of blog post files in local Jekyll blog, with YAML front matter and correctly formatted file name. Work in progress.
---
+++ myscript.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/ python3
+
+import time, os, glob, subprocess
+
+
+title = input("Title:   ")
+cat = input("Category:  ")
+tags = input("Tags:  ")
+d = time.strftime('%Y-%m-%d')
+
+path = "/Users/FlattDev/Documents/Blog/bflatt72.github.io/_posts"
+
+os.chdir(path)
+
+f = open("%s-%s.md" % (d, title), "w+")
+
+print("------", file=f)
+print("Title:%s\r" % title, file=f)
+
+if cat: print("Categories: %s\r" % cat, file=f)
+if tags: print("Tags: %s\r" % tags, file=f)
+print("------", file=f)
+f.close()
+
+list_of_files = glob.glob('/users/FlattDev/Documents/Blog/bflatt72.github.io/_posts/*.md')
+latest_file = max(list_of_files, key=os.path.getctime)
+
+subprocess.call(['code', latest_file])
+
